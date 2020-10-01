e395e49dcbc0348284fd29ae7f71ff162a9eb992
kike <kikegbc2@gmail.com>
2020-10-01 18:27:54 -0500

    mi commit

---
+++ contenido.php
@@ -0,0 +1,26 @@
+<?php 
+
+	function contenido(){
+		$datos=array();
+
+		$datos[0]="img/fuego.jpg"."||".
+				  "Fuego"."||".
+				  "Pasión, energía, fuerza, claridad, voluntad, iniciativa e impaciencia.Es el elemento que reacciona rápidamente, con entusiasmo, el que está siempre viendo posibilidades, es impetuoso e invade espacios. El fuego avanza y seduce a otros a avanzar con él.";
+
+		$datos[1]="img/tierra.jpg"."||".
+				  "Tierra"."||".
+				  "Firmeza, estabilidad, tenacidad, búsqueda de lo concreto, paciencia, cautela y seguridad.Es el elemento de la fuerza, de la resistencia a los cambios, inflexible, este elemento permite echar y mantenernos estables en relaciones, ideas, proyectos y fijar límites.";
+
+		$datos[2]="img/aire.jpg"."||".
+				  "Aire"."||".
+				  "Libertad, ideas,  cambio, desapego, perspectiva,  curiosidad y necesidad de socializar.Es el elemento de la fluidez, de la liviandad, cambia de dirección en cualquier momento. Nos permite cambiar de opinión. Es posible el sueño y el soñar. No tiene límites claros de acción y pensamiento.";
+
+		$datos[3]="img/agua.jpg"."||".
+				  "AGUA"."||".
+				  "Flexibilidad, adaptabilidad, fluidez, intimidad, necesidad de vincularse emocionalmente.Es el elemento del entendimiento, es decir, del escuchar, de ser receptivo, de la empatía, es el elemento que cede, que se adapta, retrocede y acepta.";
+
+		
+		return $datos;
+	}
+
+ ?>
